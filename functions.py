71d5c4d935709037b7309add054a3741b08f16dc
Dan Shaw <dbshaw@princeton.edu>
2018-11-21 15:03:31 -0500

    Made the approximation scheme second order with the symmetric difference quotient

--- functions.py
+++ functions.py
@@ -35,14 +35,12 @@ def approximateJacobian(f, x, dx=1e-6):
     else:
         x = float(x)
     
-    # Evaluate f(x) up front, since we'll need this value in multiple
-    # places
-    fx = f(x)
 
     # First, handle the case in which x is a scalar (i.e. not
     # array-like, just a plain number)
+    # User the second order symmetric difference quotient
     if np.isscalar(x):
-        return (f(x + dx) - fx) / dx
+        return (f(x + dx) - f(x - dx)) / (2*dx)
 
     # From this point on, x must be a numpy array or numpy matrix, so
     # Df_x will be returned as a numpy matrix. Let's initialize it as
@@ -92,7 +90,8 @@ def approximateJacobian(f, x, dx=1e-6):
     for i in range(x.size): # Could also have said range(x.size)
         h[i] = dx
         # Replace ith col of Df_x with difference quotient
-        Df_x[:,i] = (f(x + h) - fx) / dx
+        # User the second order symmetric difference quotient
+        Df_x[:,i] = (f(x + h) - f(x - h)) / (2*dx)
         # Reset h[i] to 0
         h[i] = 0
     # NOTE that there are more numpy-ish ways to iterate over the
